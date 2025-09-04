fc32010e7b10e4f28f8e4085700aceeb1c32e1cb
yeainlee <lyy923923@gmail.com>
2025-09-05 00:23:17 +0900

    :sparkles: 로그인 후 프론트로 이동, 하드코딩 된 주소 수정

--- src/main/java/com/berryselect/backend/auth/controller/AuthController.java
+++ src/main/java/com/berryselect/backend/auth/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.berryselect.backend.config.KakaoOauthProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +22,9 @@ public class AuthController {
 
     private final AuthService authService;
     private final KakaoOauthProperties kakaoProps;
+
+    @Value("${app.front.base-url}")
+    private String frontBaseUrl;
 
     // 1. 로그인 시작 : 카카오 동의창으로 리다이렉트
     @GetMapping ("/kakao/authorize")
@@ -40,11 +44,26 @@ public class AuthController {
         res.sendRedirect(url);
     }
 
-    // 2. 콜백 : code 수신 -> 토큰 교환 -> 우리 JWT 발급
+//    // 2. 콜백 : code 수신 -> 토큰 교환 -> 우리 JWT 발급
+//    @GetMapping("/kakao/callback")
+//    public ResponseEntity<?> kakaoCallback(@RequestParam("code") String code){
+//        var r = authService.loginWithAuthorizationCode(code);
+//        return ResponseEntity.ok(r);
+//    }
+
+    // 2. 콜백 : code 수신 -> 토큰 교환 -> 우리 JWT 발급 -> 프론트로 리다이렉트
     @GetMapping("/kakao/callback")
-    public ResponseEntity<?> kakaoCallback(@RequestParam("code") String code){
-        var r = authService.loginWithAuthorizationCode(code);
-        return ResponseEntity.ok(r);
+    public void kakaoCallback(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
+        var r = authService.loginWithAuthorizationCode(code); // r에 accessToken, refreshToken 포함
+
+        String base = frontBaseUrl.replaceAll("/+$", "");
+        String front = base + "/auth/callback";
+
+        String redirectUrl = front
+                + "#accessToken=" + URLEncoder.encode(r.getAccessToken(), StandardCharsets.UTF_8)
+                + "&refreshToken=" + URLEncoder.encode(r.getRefreshToken(), StandardCharsets.UTF_8);
+
+        response.sendRedirect(redirectUrl); // 프론트로 이동
     }
 
     // 로그아웃
