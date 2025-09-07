a4e638c5db73193863d66718385881f55216bfa7
yeainlee <lyy923923@gmail.com>
2025-09-07 21:13:07 +0900

    :bug: - category 겹침. auth/domain/category 삭제, merchant/domain/category 사용 - wallet/domain/StringListJsonConverter 추가 - MerchantService : brand 수정

--- src/main/java/com/berryselect/backend/merchant/domain/Category.java
+++ src/main/java/com/berryselect/backend/merchant/domain/Category.java
@@ -15,5 +15,6 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false, length = 100)
     private String name;
 }
